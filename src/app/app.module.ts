63c4ba38dffee54d4923a38bea654f480c5df3a0
Mehdi-bzi <calymed@hotmail.com>
2021-03-16 10:28:03 +0100

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { AdsService } from './services/ads/ads.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeViewComponent,
+
+  ], 
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule 
   ],
-  providers: [],
+  providers: [
+    AdsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
